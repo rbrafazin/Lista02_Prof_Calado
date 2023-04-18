b85bcc080eaecc7604ca5916c9c8d197c7e659d5
Rafael Brandão <128430012+rbrafazin@users.noreply.github.com>
2023-04-18 20:16:40 -0300

    Update and rename numeroMaior.java to NumeroMaior.java

--- src/NumeroMaior.java
+++ src/NumeroMaior.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class numeroMaior {
+public class NumeroMaior {
     public static void main(String[] args) throws Exception {
 
         Scanner sc = new Scanner(System.in);
