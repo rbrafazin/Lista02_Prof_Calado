723a5b1c4b7b5c5ba3e88c5f31b06873e1ccf9b8
Rafael Brandão <128430012+rbrafazin@users.noreply.github.com>
2023-04-18 20:15:56 -0300

    Update and rename numeroInteiro.java to NumeroInteiro.java

--- src/NumeroInteiro.java
+++ src/NumeroInteiro.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class numeroInteiro {
+public class NumeroInteiro {
     public static void main(String[] args) throws Exception {
 
         Scanner sc = new Scanner(System.in);
