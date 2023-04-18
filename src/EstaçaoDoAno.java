ed9cc4cf2ee2c12d69f32ebe9e14715ea5457057
Rafael Brandão <128430012+rbrafazin@users.noreply.github.com>
2023-04-18 20:11:47 -0300

    Update and rename estaçaoDoAno.java to EstaçaoDoAno.java

--- src/EstaçaoDoAno.java
+++ src/EstaçaoDoAno.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class estaçaoDoAno {
+public class EstaçaoDoAno {
     public static void main(String[] args) throws Exception {
 
         Scanner sc = new Scanner(System.in);
