9faecc7623f5705c1890541a2f5dfbe7ceaa78e3
Rafael Brandão <128430012+rbrafazin@users.noreply.github.com>
2023-04-18 20:17:13 -0300

    Update and rename ordemCrescente.java to OrdemCrescente.java

--- src/OrdemCrescente.java
+++ src/OrdemCrescente.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class ordemCrescente {
+public class OrdemCrescente {
     public static void main(String[] args) throws Exception {
 
         Scanner sc = new Scanner(System.in);
