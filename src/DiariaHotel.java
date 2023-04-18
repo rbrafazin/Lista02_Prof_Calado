469affbc1f82fbeefdebd8e13a4d35724521ace1
Rafael Brandão <128430012+rbrafazin@users.noreply.github.com>
2023-04-18 20:08:57 -0300

    Update DiariaHotel.java

--- src/DiariaHotel.java
+++ src/DiariaHotel.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class diariaHotel {
+public class DiariaHotel {
     public static void main(String[] args) throws Exception {
 
         Scanner sc = new Scanner(System.in);
